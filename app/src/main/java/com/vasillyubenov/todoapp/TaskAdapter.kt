dec0f65c5fbc9b01f7b16a703cbb7e33c044d625
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2025-06-12 21:28:01 +0300

    removed semicolon

--- app/src/main/java/com/vasillyubenov/todoapp/TaskAdapter.kt
+++ app/src/main/java/com/vasillyubenov/todoapp/TaskAdapter.kt
@@ -10,19 +10,19 @@ class TaskAdapter (private val taskList: List<Task>) :
     RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
 
     class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val taskTitle: TextView = view.findViewById(R.id.taskText);
+        val taskTitle: TextView = view.findViewById(R.id.taskText)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.task_item, parent, false);
-        return TaskViewHolder(view);
+            .inflate(R.layout.task_item, parent, false)
+        return TaskViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        val task = taskList[position];
-        holder.taskTitle.text = task.title;
+        val task = taskList[position]
+        holder.taskTitle.text = task.title
     }
 
-    override fun getItemCount(): Int = taskList.size;
+    override fun getItemCount(): Int = taskList.size
 }
