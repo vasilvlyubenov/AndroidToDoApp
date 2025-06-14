7950f5bda4d524e63e9d472327041e8bde671aa6
Vasil Lyubenov <vasil.lyubenov@gmail.com>
2025-06-14 20:14:04 +0300

    feat: adding task complete

--- app/src/main/java/com/vasillyubenov/todoapp/MainActivity.kt
+++ app/src/main/java/com/vasillyubenov/todoapp/MainActivity.kt
@@ -1,28 +1,39 @@
 package com.vasillyubenov.todoapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var taskAdapter: TaskAdapter
-    private lateinit var taskList: MutableList<Task>
+    private var taskList = mutableListOf<Task>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        taskList = mutableListOf(
-            Task("Buy groceries"),
-            Task("Walk the dog"),
-            Task("Learn Kotlin")
-        )
+        val recyclerView = findViewById<RecyclerView>(R.id.taskRecyclerView)
+        val taskInput = findViewById<EditText>(R.id.taskInput)
+        val addButton = findViewById<Button>(R.id.addButton)
 
         taskAdapter = TaskAdapter(taskList)
-
-        val recyclerView = findViewById<RecyclerView>(R.id.taskRecyclerView)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = taskAdapter
+
+        addButton.setOnClickListener {
+            val taskText = taskInput.text.toString()
+            if (taskText.isNotBlank()) {
+                val task = Task(taskText)
+                taskList.add(task)
+                taskInput.text.clear()
+            } else {
+                Toast.makeText(this, "Please enter a task", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     }
 }
